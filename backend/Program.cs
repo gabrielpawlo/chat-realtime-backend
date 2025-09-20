f5eb01fcda870bb8363fa02cd1a7d8fd1e7e11db
Gabriel <gabrielgpawlowski@gmail.com>
2025-09-20 18:51:32 -0300

    adicionando url para o cors

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,20 +1,17 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Adiciona os serviços essenciais
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// Adiciona o serviço SignalR
 builder.Services.AddSignalR();
 
-// Configura a política de CORS
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("MyCorsPolicy",
         policy =>
         {
-            policy.WithOrigins("https://gabrielpawlo.github.io/chat-realtime")
+            policy.WithOrigins("https://gabrielpawlo.github.io/chat-realtime") // CORREÇÃO AQUI
                   .AllowAnyHeader()
                   .AllowAnyMethod();
         });
@@ -22,7 +19,6 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
-// Configura o pipeline HTTP
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -32,15 +28,11 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.UseRouting();
-
-// Usa a política de CORS
 app.UseCors("MyCorsPolicy");
 
-// Mapeia os endpoints
 app.MapControllers();
 app.MapHub<ChatApp.Hubs.ChatHub>("/chatHub");
 
-// Configura o uso de arquivos estáticos
 app.UseDefaultFiles();
 app.UseStaticFiles();
 

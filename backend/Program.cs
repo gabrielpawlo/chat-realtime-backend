eea99202c7684f9815e5d39e3d0904aef0de17f2
Gabriel <gabrielgpawlowski@gmail.com>
2025-09-20 18:37:07 -0300

    colocando url do github pages

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,17 +1,20 @@
 var builder = WebApplication.CreateBuilder(args);
 
+// Adiciona os serviços essenciais
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Adiciona o serviço SignalR
 builder.Services.AddSignalR();
 
+// Configura a política de CORS
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("MyCorsPolicy",
         policy =>
         {
-            policy.AllowAnyOrigin()
+            policy.WithOrigins("https://gabrielpawlo.github.io/chat-realtime")
                   .AllowAnyHeader()
                   .AllowAnyMethod();
         });
@@ -19,6 +22,7 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
+// Configura o pipeline HTTP
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -29,11 +33,14 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 app.UseRouting();
 
+// Usa a política de CORS
 app.UseCors("MyCorsPolicy");
 
+// Mapeia os endpoints
 app.MapControllers();
 app.MapHub<ChatApp.Hubs.ChatHub>("/chatHub");
 
+// Configura o uso de arquivos estáticos
 app.UseDefaultFiles();
 app.UseStaticFiles();
 
